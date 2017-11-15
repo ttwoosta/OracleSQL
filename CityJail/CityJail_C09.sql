eed47e284fcec5ebced1a71f6d0c3e6f7e2f90c7
Tu Tong <ttwoosta@gmail.com>
2017-11-14 21:42:51 -0500

    Working on Chapter 9 city jail homework

---
+++ CityJail/CityJail_C09.sql
@@ -0,0 +1,55 @@
+/* 
+Script Name:  Chapter 9 City Jail
+Developer:    Tu Tong
+Date:         November 2, 2017
+Purpose:      Joining Data from Multiple Tables
+*/
+
+/*
+For each request, include one solution using the traditional method and one 
+using an ANSI JOIN statement.
+*/
+
+/*
+1. List all criminals along with the crime charges filed. The report needs to
+include the criminal ID, name, crime code, and fine amount.
+*/
+
+
+
+/*
+2. List all criminals along with crime status and appeal status (if applicable).
+The resports need to include the criminal ID, name, crime classification, date
+charged, appeal filing date, and appeal status. Show all criminals, regardless
+of whether they have filed an appeal.
+*/
+
+/*
+3. List all criminals along with crime information. The report needs to include
+the criminal ID, name, crime classification, date charged, crime code, and fine
+amount. Include only crimes classified as "Other." Sort the list by criminal ID,
+and date charged.
+*/
+
+/*
+4. Create an alphabetical list of all criminals, including criminal ID, name, 
+violent offender status, parole status, and any known aliases. 
+*/
+
+/*
+5. A table named Prob_Contact contains the required frequency of contact with a
+probation officer, based on the length of the probation period (the number of 
+days assigned to probation). Review the data in this table, which indicates
+ranges for the number of days and applicable contact frequencies. Create a list
+containing the name of each criminal who has been assigned a probation period,
+which is indicated by the sentence type. The list should contain the criminal
+name, probation start date, probation end date, and required frequency of contact.
+Sort the list by criminal name and probation start date.
+*/
+
+/*
+6. A column named Mgr_ID has beed added to Prob_Officers table and contains the
+ID number of the probation supervisor for each officer. Produce a list showing
+each probation officer's name and his or her supervisor's name. Sort the list
+alphabetically by probation officer name.
+*/
